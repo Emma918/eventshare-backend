706e828393d1b5fffc5d584729d82fb1e5491588
Yang Zhennan <jenyang0918@gmail.com>
2024-09-19 20:22:28 +1200

    Install dotenv and update server.js to use environment variables

--- server.js
+++ server.js
@@ -6,19 +6,30 @@ const eventRoutes = require('./routes/eventRoutes');
 const normalUserRoutes = require('./routes/userDetailRoutes');
 const columnRoutes = require('./routes/columnRoutes');
 const app = express();
+
+// 使用 dotenv 来加载环境变量
+require('dotenv').config();
+
 app.use(cors());
 app.use(express.json()); 
 
-// 路由等设置 
+// 路由等设置
 app.use('/auth', authRoutes);
 app.use('/api', eventRoutes);
 app.use('/api', normalUserRoutes);
 app.use('/api', columnRoutes);
-app.use('/uploads', express.static('uploads'))
-mongoose.connect('mongodb://localhost/EventShare', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Database connected'))
-  .catch(err => console.log(err));
+app.use('/uploads', express.static('uploads'));
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+// 使用环境变量连接 MongoDB
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log('Database connected'))
+.catch(err => console.log('Database connection error:', err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
