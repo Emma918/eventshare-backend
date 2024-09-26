cea273a49110665458fc6f7fd6e7c85b35a39f24
Emma918 <66978478+Emma918@users.noreply.github.com>
2024-09-26 22:35:43 +1200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,17 @@ const columnRoutes = require('./routes/columnRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ['https://eventshare-front-zhennan-yangs-projects.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+}));
 app.use(express.json()); 
 
 // 路由等设置
